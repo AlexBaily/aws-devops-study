b933dae0f5d4b7e46f9e2e1d62477be13930e02d
EC2 Default User <ec2-user@ip-10-0-0-102.eu-west-1.compute.internal>
2018-03-13 19:43:35 +0000

    add initial vpc resource, internet gateway and public subnet

--- terraform/modules/vpc/vpc.tf
+++ terraform/modules/vpc/vpc.tf
@@ -1,7 +1,26 @@
-#Terraform VPC Module
+#Terraform Network Module
 
+variable "name" {}
+variable "vpc_cidr" {}
+variable "cidrs" {}
 
 resource "aws_vpc" "vpc" {
- 
+  cidr_block = "${var.vpc_cidr}"
+  
+  tags { Name = "${var.environment_name}" } 
 
+}
+
+resource "aws_internet_gateway" "public" {
+  vpc_id = "${vpc.vpc_id}"
+
+  tags { Name = "${var.environment_name}" }
+}
+
+
+resource "aws_subnet" "public" {
+  vpc_id            = "${vpc.vpc_id}"
+  cidr_block        = "${element(split(",", var.cidrs), count.index)}"
+  availability_zone = "${element(split(",", var.azs), count.index)}"
+  count             = "${length(split(",", var.cidrs))}"
 }
