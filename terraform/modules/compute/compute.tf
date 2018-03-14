65b5ab8c0da168492623ccc5741259a46ef248c0
EC2 Default User <ec2-user@ip-10-0-0-102.eu-west-1.compute.internal>
2018-03-14 15:21:15 +0000

    add resources that lay the structure for the eventual ASGs

--- terraform/modules/compute/compute.tf
+++ terraform/modules/compute/compute.tf
@@ -1,1 +1,23 @@
 #This module will be used for the EC2 ASGs
+
+variable "region"  = {}
+variable "ami-id"  = {}
+variable "subnets" = {}
+
+data "aws_ami" "nodejs" {
+
+}
+
+resource "aws_launch_configuration" "as_conf" {
+
+
+}
+
+resource "aws_autoscaling_group" "as_group" {
+
+
+}
+
+resource "aws_autoscaling_policy" "as_policy" {
+
+}
